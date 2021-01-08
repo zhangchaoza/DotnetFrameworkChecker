84597c320c57a1417a0edd66d193632d5c9062fd
zhangchao <zackability@hotmial.com>
2021-01-08 11:01:10 +0800

    fix: fix error when not installed net45 or or later version

--- src/get_dotnet_version.rs
+++ src/get_dotnet_version.rs
@@ -5,42 +5,44 @@ use winreg::RegKey;
 pub fn get_45_plus_from_registry() {
     let npd_sub_key = r#"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\"#;
 
-    let ndp_key = RegKey::predef(HKEY_LOCAL_MACHINE)
-        .open_subkey(npd_sub_key)
-        .unwrap_or_else(|e| match e.kind() {
+    let ndp_key_option = RegKey::predef(HKEY_LOCAL_MACHINE).open_subkey(npd_sub_key);
+    if ndp_key_option.is_err() {
+        match ndp_key_option.unwrap_err().kind() {
             io::ErrorKind::NotFound => {
-                panic!(".NET Framework Version 4.5 or later is not detected.")
+                println!(".NET Framework Version 4.5 or later is not detected.")
             }
-            io::ErrorKind::PermissionDenied => panic!("Access denied"),
-            _ => panic!("{:?}", e),
-        });
-
-    let release_key: u32 = ndp_key
-        .get_value("Release")
-        .unwrap_or_else(|e| match e.kind() {
-            io::ErrorKind::NotFound => {
-                panic!(".NET Framework Version 4.5 or later is not detected.")
-            }
-            io::ErrorKind::PermissionDenied => panic!("Access denied"),
-            _ => panic!("{:?}", e),
-        });
-
-    let version = match release_key {
-        r if r >= 528040 => "4.8 or later",
-        r if r >= 461808 => "4.7.2",
-        r if r >= 461308 => "4.7.1",
-        r if r >= 460798 => "4.7",
-        r if r >= 394802 => "4.6.2",
-        r if r >= 394254 => "4.6.1",
-        r if r >= 393295 => "4.6",
-        r if r >= 379893 => "4.5.2",
-        r if r >= 378675 => "4.5.1",
-        r if r >= 378389 => "4.5",
-        // This code should never execute. A non-null release key should mean
-        // that 4.5 or later is installed.
-        _ => "No 4.5 or later version detected",
-    };
-    println!(".NET Framework Version: {}", version);
+            _x => println!("{:?}", _x),
+        };
+    } else {
+        let ndp_key = ndp_key_option.unwrap();
+        let release_key_option = ndp_key.get_value("Release");
+        if release_key_option.is_err() {
+            match release_key_option.unwrap_err().kind() {
+                io::ErrorKind::NotFound => {
+                    println!(".NET Framework Version 4.5 or later is not detected.")
+                }
+                _x => println!("{:?}", _x),
+            };
+        } else {
+            let release_key: u32 = release_key_option.unwrap();
+            let version = match release_key {
+                r if r >= 528040 => "4.8 or later",
+                r if r >= 461808 => "4.7.2",
+                r if r >= 461308 => "4.7.1",
+                r if r >= 460798 => "4.7",
+                r if r >= 394802 => "4.6.2",
+                r if r >= 394254 => "4.6.1",
+                r if r >= 393295 => "4.6",
+                r if r >= 379893 => "4.5.2",
+                r if r >= 378675 => "4.5.1",
+                r if r >= 378389 => "4.5",
+                // This code should never execute. A non-null release key should mean
+                // that 4.5 or later is installed.
+                _ => "No 4.5 or later version detected",
+            };
+            println!(".NET Framework Version: {}", version);
+        }
+    }
 }
 
 pub fn get_version_from_registry() {
